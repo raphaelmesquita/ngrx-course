7a005c2e4d1e376778d9580284d0a76c77eb0b84
Raphael Lima <rlima@gaveainvest.com.br>
2018-02-15 10:32:29 -0200

    Implementaçãod e unreadMessagesCounter$

--- src/app/thread-section/thread-section.component.ts
+++ src/app/thread-section/thread-section.component.ts
@@ -1,5 +1,7 @@
 import { Store } from "@ngrx/store";
 import { Component, OnInit } from "@angular/core";
+import * as _ from "lodash";
+
 import { ThreadsService } from "../services/threads.service";
 import { ApplicationState } from "../store/application-state";
 import { LoadUserThreadsAction } from "../store/actions";
@@ -14,15 +16,29 @@ import { Observable } from "rxjs";
 export class ThreadSectionComponent implements OnInit {
 
   userName$: Observable<string>;
+  unreadMessagesCounter$: Observable<number>;
 
   constructor(private threadsService: ThreadsService, private store: Store<ApplicationState>) {
-    this.userName$ = this.store.pipe(
-      skip(1),
-      map(this.mapStateToUserName));
+    this.userName$ =
+      this.store.pipe(
+        skip(1),
+        map(this.mapStateToUserName));
+
+    this.unreadMessagesCounter$ =
+      this.store.pipe(
+        skip(1),
+        map(this.mapStateToUnreadMessagesCounter)
+      )
   }
 
   mapStateToUserName(state: ApplicationState): string {
     return state.storeData.participants[state.uiState.userId].name;
+  }
+
+  mapStateToUnreadMessagesCounter(state: ApplicationState): number {
+    let currentUserId = state.uiState.userId;
+    return _.values(state.storeData.threads)
+      .reduce((acc, thread) => acc + thread.participants[currentUserId], 0);
   }
 
   ngOnInit() {
